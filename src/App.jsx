7fe9d8133c6baa3ae4935db1098b5b075750e4b0
FutureOneX <shamilkhangumusel@gmail.com>
2023-10-17 16:25:06 +0200

    Splash&SignInPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EventPage from './components/pages/EventPage';
 import Protected from './components/shared/Protected';
-import SplashScreenPage from './components/pages/SplashScreenPage';
+import SplashScreenPage from './components/pages/SplashScreenPage.jsx';
+import SignInPage from './components/pages/SignInPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SplashScreenPage />} />
+        <Route path='/signin' element={<SignInPage/>} />
         <Route element={<Protected />}>
+
           <Route path="/event" element={<EventPage />} />
         </Route>
       </Routes>
