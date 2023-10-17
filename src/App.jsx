916e3759961ff0538e4cf8d5769eade9b5861e6e
Matthew7991 <Matthew7991@gmx.de>
2023-10-17 13:28:27 +0200

    backButton folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EventPage from './components/pages/EventPage';
 import Protected from './components/shared/Protected';
 import SplashScreenPage from './components/pages/SplashScreenPage';
-import BackButton from './components/ui/BackButton.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SplashScreenPage />} />
-        <Route path="/test" element={<BackButton />} />
         <Route element={<Protected />}>
           <Route path="/event" element={<EventPage />} />
         </Route>
