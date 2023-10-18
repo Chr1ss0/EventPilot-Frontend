f497e2bf580b4b94289f758e5ae172e89761f5a3
DanielKirchenbauer <kirchenbauer.daniel@gmail.com>
2023-10-18 09:49:00 +0200

    MainButton Conclusion

--- src/components/ui/MainButton.jsx
+++ src/components/ui/MainButton.jsx
@@ -3,14 +3,24 @@ import CSSModules from 'react-css-modules';
 import styles from './MainButton.module.css';
 import ArrowIcon from '../../assets/img/Arrow.svg';
 
-function MainButton({ children }) {
+function MainButton({ children, showArrow = true }) {
   return (
     <>
       <div className={styles.buttonDiv}>
         <button className={styles.mainButton}>
           <div className={styles.mainButtonDiv}>
-            <p className={styles.mainButtonText}>{children}</p>
-            <img className={styles.arrowIcon} src={ArrowIcon} alt="arrow" />
+            <p
+              className={
+                showArrow
+                  ? styles.mainButtonText
+                  : styles.mainButtonWithoutArrow
+              }
+            >
+              {children}
+            </p>
+            {showArrow && (
+              <img className={styles.arrowIcon} src={ArrowIcon} alt="arrow" />
+            )}
           </div>
         </button>
       </div>
