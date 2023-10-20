d05e63819a0735023be5454c914bde8a5b99f71d
FutureOneX <shamilkhangumusel@gmail.com>
2023-10-20 11:28:26 +0200

    EventItemRow

--- src/components/explore/EventItemRow.jsx
+++ src/components/explore/EventItemRow.jsx
@@ -1,21 +1,34 @@
 import titlePicture from '../../assets/img/EventImg.svg'
 import styles from './EventItemRow.module.css'
+import pin from '../../assets/img/Location.svg'
+import BookmarkButton from '../ui/BookmarkButton'
+
 
 function EventItemRow() {
   return (
   <div>
     <div className={styles['eventContainer']}>
       <div className={styles['eventContentContainer']}>
+        <div className={styles['bookmarkButtonContainer']}>
+          <BookmarkButton/>
+        </div>
         <img src={titlePicture} alt="titlepicture" />
-        <p>Lorem Date</p>
+          <div className={styles['EventDateContainer']}>
+            <p>18 <span>July</span></p>
+            {/* Date Lgoic */}
           </div>
-          <div className={styles['eventRowInfoContainer']}>
-          <h4>Lorem Ipsum Party</h4>
-          <div className={styles['eventDateVisitorContainer']}>
-          <p>Lorem Guesst</p>
-          <img src="" alt="" />
-          <p>Lorem, Ort</p>
-          </div>
+      </div>
+          <div className={styles['eventRowInfoContainerWraper']}>
+            <h4>Lorem Ipsum Party</h4>
+              <div className={styles['eventPlaceVisitorContainer']}>
+                <p>99+</p>
+                {/* Visitors Logic */}
+                <div className={styles['eventPlaceContainer']}>
+                  <img src={pin} alt="Pin" />
+                  <p>Lorem, Place</p>
+                  {/* Place Logic */}
+                </div>
+              </div>
           </div>
         <div>
       </div>
