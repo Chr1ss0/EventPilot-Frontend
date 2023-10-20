1be8ac7f7f81134e66f6b9f1bf8ce8220df760cf
FutureOneX <shamilkhangumusel@gmail.com>
2023-10-20 08:46:28 +0200

    splashscreentimer

--- src/components/explore/EventItemRow.jsx
+++ src/components/explore/EventItemRow.jsx
@@ -1,5 +1,27 @@
+import titlePicture from '../../assets/img/EventImg.svg'
+import styles from './EventItemRow.module.css'
+
 function EventItemRow() {
-  return <></>;
+  return (
+  <div>
+    <div className={styles['eventContainer']}>
+      <div className={styles['eventContentContainer']}>
+        <img src={titlePicture} alt="titlepicture" />
+        <p>Lorem Date</p>
+          </div>
+          <div className={styles['eventRowInfoContainer']}>
+          <h4>Lorem Ipsum Party</h4>
+          <div className={styles['eventDateVisitorContainer']}>
+          <p>Lorem Guesst</p>
+          <img src="" alt="" />
+          <p>Lorem, Ort</p>
+          </div>
+          </div>
+        <div>
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default EventItemRow;
