7bbccb710c639a7bdc0e1c9f828b5b1605bb3db5
DanielKirchenbauer <kirchenbauer.daniel@gmail.com>
2023-10-23 12:46:37 +0200

    FilterButton

--- src/components/pages/Test.jsx
+++ src/components/pages/Test.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import EventItemRow from '../explore/EventItemRow';
 import ProfileInfo from '../profile/ProfileInfo';
+import FilterButton from '../search/FilterButton';
 
 export default function Test() {
   return (
     <div>
-      <ProfileInfo vertical={false}></ProfileInfo>
+      <FilterButton />
     </div>
   );
 }
