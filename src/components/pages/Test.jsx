fbcf3b8f9dc9056b8d92022707e252d1346d0d70
DanielKirchenbauer <kirchenbauer.daniel@gmail.com>
2023-10-24 12:08:53 +0200

    FilterCategoryButton

--- src/components/pages/Test.jsx
+++ src/components/pages/Test.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import FilterMenuButton from '../search/FilterMenuButton';
 
 export default function Test() {
-  return <></>;
+  return (
+    <>
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <FilterMenuButton category='sports' />
+        <FilterMenuButton category='music' />
+        <FilterMenuButton category='art' />
+        <FilterMenuButton category='food' />
+      </div>
+    </>
+  );
 }
