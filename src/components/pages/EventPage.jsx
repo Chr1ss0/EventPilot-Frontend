e09086a23b736d3f970e13ab9558491863df64bc
DanielKirchenbauer <kirchenbauer.daniel@gmail.com>
2023-10-24 17:07:25 +0200

    EventPage & Update LinkedEvents

--- src/components/pages/EventPage.jsx
+++ src/components/pages/EventPage.jsx
@@ -1,5 +1,27 @@
+import LikedEvents from '../ui/LikedEvents.jsx';
+import MainButton from '../ui/MainButton.jsx';
+import style from './EventPage.module.css';
+
 function EventPage() {
-  return <></>;
+  return (
+    <>
+      <main className={style.main}>
+        <article className={style.upper}>
+          <h1>Upcoming Events</h1>
+          <LikedEvents />
+        </article>
+        <article className={style.middle}>
+          <div className={style.noEvents}>
+            <h2>No Upcoming Events</h2>
+            <p>There are no Events yet!</p>
+          </div>
+        </article>
+        <article className={style.bottom}>
+          {/* <MainButton>search events</MainButton> */}
+        </article>
+      </main>
+    </>
+  );
 }
 
 export default EventPage;
--- src/components/ui/LikedEvents.jsx
+++ src/components/ui/LikedEvents.jsx
@@ -1,15 +1,48 @@
-import styles from './LikedEvents.module.css'
+import { useState } from 'react';
+import styles from './LikedEvents.module.css';
 
 function LikedEvents() {
+  const [active, setActive] = useState(false);
+  function clickButton() {
+    event.preventDefault();
+    setActive((current) => !current);
+  }
+
   return (
-  <div className={styles['likedEventsWrapper']}>
-    <div className={styles['upcomingEventsWrapper']}>
-    <p>UPCOMING</p>
+    <div className={styles['likedEventsWrapper']}>
+      {active ? (
+        <button
+          onClick={clickButton}
+          className={styles['upcomingEventsWrapper']}>
+          BOOKMARKED
+        </button>
+      ) : (
+        <button
+          onClick={clickButton}
+          className={styles.left}>
+          BOOKMARKED
+        </button>
+      )}
+      {/* <div className={styles['upcomingEventsWrapper']}>
+        <p>UPCOMING</p>
+      </div> */}
+      {active ? (
+        <button
+          onClick={clickButton}
+          className={styles.right}>
+          REGISTERED
+        </button>
+      ) : (
+        <button
+          onClick={clickButton}
+          className={styles['pastEventsWrapper']}>
+          REGISTERED
+        </button>
+      )}
+      {/* <div className={styles['pastEventsWrapper']}>
+        <p>PAST EVENENTS</p>
+      </div> */}
     </div>
-    <div className={styles['pastEventsWrapper']}>
-    <p>PAST EVENENTS</p>
-    </div>
-  </div>
   );
 }
 
