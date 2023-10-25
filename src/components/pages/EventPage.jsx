602d6e3296d788854526f65af68684670f90105d
Matthew7991 <Matthew7991@gmx.de>
2023-10-25 07:42:18 +0200

    added all components to pages; created Rating comp; fixed SVG errors

--- src/components/pages/EventPage.jsx
+++ src/components/pages/EventPage.jsx
@@ -1,8 +1,30 @@
 import LikedEvents from '../ui/LikedEvents.jsx';
 import MainButton from '../ui/MainButton.jsx';
+import EventItemCol from '../shared/EventItemCol.jsx';
+import EventListCol from '../layout/EventListCol.jsx';
+
 import style from './EventPage.module.css';
+import { useEffect, useState } from 'react';
 
 function EventPage() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function getEvents() {
+      const response = await fetch(
+        import.meta.env.VITE_BACKEND_URL + '/api/event/all',
+        { credentials: 'include' },
+      );
+      // console.log(response);
+      const result = await response.json();
+      // console.log(result);
+
+      if (!response.ok) return;
+      setEvents(result);
+    }
+    getEvents();
+  }, []);
+
   return (
     <>
       <main className={style.main}>
@@ -14,6 +36,16 @@ function EventPage() {
           <div className={style.noEvents}>
             <h2>No Upcoming Events</h2>
             <p>There are no Events yet!</p>
+          </div>
+          <div>
+            <EventListCol>
+              {events.map((event) => (
+                <EventItemCol
+                  key={event._id}
+                  event={event}
+                />
+              ))}
+            </EventListCol>
           </div>
         </article>
         <article className={style.bottom}>
