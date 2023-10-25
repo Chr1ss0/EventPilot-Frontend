5d5ad1af95202278e3ffb2814fdaf6181aabb251
Chr1ss0 <cb994@web.de>
2023-10-25 16:07:32 +0200

    added locationfilter second try

--- src/components/search/FilterLocationMenu.jsx
+++ src/components/search/FilterLocationMenu.jsx
@@ -1,21 +1,55 @@
 import { useState } from 'react';
+import style from './FilterLocationMenu.module.css';
+import filterLocationIcon from '../../assets/img/filterLocation.svg';
+import userLocationIcon from '../../assets/img/userLocation.svg';
+import filterLocationArrow from '../../assets/img/filterSvg.svg';
 
-export default function SearchLocationMenu() {
-  const [searchLocation, setSearchLocation] = useState({
-    place: 'Stadt',
-    state: 'Bundesland',
-  });
+export default function FilterLocationMenu() {
+  const [searchLocation, setSearchLocation] = useState('Stadt');
+  const [switchFields, setSwitchFields] = useState(false);
+
+  function setUserLocation() {}
+
+  function setZipLocation() {
+    setSwitchFields(false);
+  }
+
   return (
-    <section>
-      <p>Location</p>
-      <div>
-        <div>Svg</div>
-        <p>
-          `${searchLocation.place}, ${searchLocation.state}`
-        </p>
-        <div>Arrow</div>
-        <div>UserImg</div>
+    <div className={style.filterLocationMenu_wrapper}>
+      <div className={style.filterLocationMenu_input}>
+        <img
+          className={style.filterLocationMenu_searchIcon}
+          onClick={() => setSwitchFields((prevState) => !prevState)}
+          src={filterLocationIcon}
+          alt='set Userlocation button'
+        />
+        {switchFields ? (
+          <input
+            className={style.filterLocationMenu_currentLocation}
+            type='text'
+            placeholder={'21514'}
+          />
+        ) : (
+          <p className={style.filterLocationMenu_currentLocation}>
+            {searchLocation}
+          </p>
+        )}
+        <img
+          onClick={setZipLocation}
+          className={style.filterLocationMenu_cta}
+          src={filterLocationArrow}
+          alt='select Zipcode location'
+        />
       </div>
-    </section>
+      <div
+        onClick={setUserLocation}
+        className={style.filterLocationMenu_usericon_wrapper}>
+        <img
+          className={style.filterLocationMenu_usericon}
+          src={userLocationIcon}
+          alt='select Location button'
+        />
+      </div>
+    </div>
   );
 }
