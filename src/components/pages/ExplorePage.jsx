1061e214d5882961da507dc389f592124ea8e07c
Matthew7991 <Matthew7991@gmx.de>
2023-10-25 13:15:33 +0200

    explorePage fetches

--- src/components/pages/ExplorePage.jsx
+++ src/components/pages/ExplorePage.jsx
@@ -4,27 +4,45 @@ import EventItemRow from '../explore/EventItemRow.jsx';
 import CurrentLocation from '../shared/CurrentLocation.jsx';
 import EventItemCol from '../shared/EventItemCol.jsx';
 import EventListCol from '../layout/EventListCol.jsx';
-import styling from './ExplorePage.module.css'
+import styling from './ExplorePage.module.css';
 
 import logo from '../../assets/img/Logo.svg';
 
 function ExplorePage() {
-  const [events, setEvents] = useState([]);
+  const [eventsUpcoming, setEventsUpcoming] = useState(null);
+  const [eventsNearby, setEventsNearby] = useState(null);
 
   useEffect(() => {
-    async function getEvents() {
+    async function getEventsUpcoming() {
       const response = await fetch(
         import.meta.env.VITE_BACKEND_URL + '/api/event/all',
         { credentials: 'include' },
       );
       // console.log(response);
       const result = await response.json();
-      // console.log(result);
+      console.log(result);
 
       if (!response.ok) return;
-      setEvents(result);
+      setEventsUpcoming(result);
     }
-    getEvents();
+    getEventsUpcoming();
+  }, []);
+
+  useEffect(() => {
+    async function getEventsNearby() {
+      const response = await fetch(
+        import.meta.env.VITE_BACKEND_URL +
+          `/api/event/filtered?location=${'user'}`,
+        { credentials: 'include' },
+      );
+      // console.log(response);
+      const result = await response.json();
+      console.log(result);
+
+      if (!response.ok) return;
+      setEventsNearby(result);
+    }
+    getEventsNearby();
   }, []);
 
   return (
@@ -36,38 +54,44 @@ function ExplorePage() {
         />
         <CurrentLocation />
       </div>
-      <div className={styling['upcomingEventsWrapper']}>
-        <h2>Upcoming Events</h2>
-        <EventListRow>
-          {events.map((event) => (
-            <EventItemRow
-              event={event}
-              key={event._id}
-            />
-          ))}
-        </EventListRow>
-      </div>
-      <div className={styling['eventListRowWrapper']}>
-        <h2>Nearby you</h2>
-        <EventListRow>
-          {events.map((event) => (
-            <EventItemRow
-              event={event}
-              key={event._id}
-            />
-          ))}
-        </EventListRow>
-      </div>
-      <div className={styling['eventListColWrapper']}>
-        <EventListCol>
-          {events.map((event) => (
-            <EventItemCol
-              key={event._id}
-              event={event}
-            />
-          ))}
-        </EventListCol>
-      </div>
+      {eventsUpcoming && (
+        <div className={styling['upcomingEventsWrapper']}>
+          <h2>Upcoming Events</h2>
+          <EventListRow>
+            {eventsUpcoming.map((event) => (
+              <EventItemRow
+                event={event}
+                key={event._id}
+              />
+            ))}
+          </EventListRow>
+        </div>
+      )}
+      {eventsNearby && (
+        <div className={styling['eventListRowWrapper']}>
+          <h2>Nearby you</h2>
+          <EventListRow>
+            {eventsNearby.map((event) => (
+              <EventItemRow
+                event={event}
+                key={event._id}
+              />
+            ))}
+          </EventListRow>
+        </div>
+      )}
+      {eventsUpcoming && (
+        <div className={styling['eventListColWrapper']}>
+          <EventListCol>
+            {eventsUpcoming.map((event) => (
+              <EventItemCol
+                key={event._id}
+                event={event}
+              />
+            ))}
+          </EventListCol>
+        </div>
+      )}
     </div>
   );
 }
