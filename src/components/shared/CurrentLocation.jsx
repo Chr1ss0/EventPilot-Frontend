937f28541f86df89b39eadf40a42c14220cb7c52
DanielKirchenbauer <kirchenbauer.daniel@gmail.com>
2023-10-25 11:31:14 +0200

    upper SearchPage Styling

--- src/components/shared/CurrentLocation.jsx
+++ src/components/shared/CurrentLocation.jsx
@@ -1,10 +1,16 @@
-import styles from './CurrentLocation.module.css'
+import styles from './CurrentLocation.module.css';
 
-function CurrentLocation() {
-  return( 
-  <div className={styles['currentLocationWrapper']}>
-    <p>Current Location</p>
-    <p>Miami, USA</p>
-  </div>);
+function CurrentLocation({ light = true }) {
+  return (
+    <div className={styles['currentLocationWrapper']}>
+      <p
+        className={
+          light ? styles.lightCurrentLocation : styles.darkCurrentLocation
+        }>
+        Current Location
+      </p>
+      <p className={light ? styles.lightTown : styles.darkTown}>Miami, USA</p>
+    </div>
+  );
 }
 export default CurrentLocation;
